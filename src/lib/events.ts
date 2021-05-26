75031f81c116cb7d1538ea8dcf5ebe002d323ac2
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-05-26 17:29:28 -0300

    Create events.ts

---
+++ src/lib/events.ts
@@ -0,0 +1,21 @@
+import { createPubSub, PubSub } from 'lightcast'
+import { Asset } from './asset'
+import { ProgressPayload } from './preloadAsset'
+
+export type PreloaderEvents = {
+  onProgress: PubSub<ProgressPayload>
+  onComplete: PubSub<Array<Asset>>
+  onFetched: PubSub<Asset>
+  onError: PubSub<Asset>
+  onCancel: PubSub<Array<Asset>>
+}
+
+export const createEvents = (): PreloaderEvents => {
+  return {
+    onProgress: createPubSub(),
+    onComplete: createPubSub(),
+    onFetched: createPubSub(),
+    onError: createPubSub(),
+    onCancel: createPubSub(),
+  }
+}
