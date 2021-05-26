4cd043b2f81aa634e84c1ff181dcd27c43b6d617
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-05-26 10:38:53 -0300

    Update index.tsx

--- example/index.tsx
+++ example/index.tsx
@@ -1,1 +1,29 @@
-const example = () => {};
+import { preloader } from '../src/'
+
+const loader = preloader()
+
+loader
+  .fetch([
+    'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
+    'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+  ])
+  // use the promise or the onComplete event
+  .then((items) => {
+    console.log(items)
+  })
+
+loader.onComplete((items) => {
+  console.log(items)
+})
+
+loader.onProgress((event) => {
+  console.log(event.progress + '%')
+})
+
+loader.onFetched((item) => {
+  console.log(item)
+})
+
+loader.onError((item) => {
+  console.log(item)
+})
