1c5c9385f0ad92c5211b4e012659b810eeaeb447
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-05-26 17:29:42 -0300

    Update preloadAsset.ts

--- src/lib/preloadAsset.ts
+++ src/lib/preloadAsset.ts
@@ -1,59 +1,89 @@
-import { Asset } from './preloader'
+export type Asset = {
+  xhr: XMLHttpRequest
+  blobUrl: string | null
+  progress: number
+  downloaded: number
+  error: boolean
+  fileName: string
+  size: number | null
+  status: number
+  total: number
+  type: string
+  url: string
+}
+
+export type ProgressPayload = {
+  asset: Asset
+  progress: number
+  event: ProgressEvent<EventTarget>
+}
 
 type PreloadAssetProps = {
-  onProgress: (event: ProgressEvent<EventTarget>) => void
+  url: string
+  onProgress: (payload: ProgressPayload) => void
   onError: (asset: Asset) => void
   onComplete: (asset: Asset) => void
-  asset: Asset
   responseType?: XMLHttpRequestResponseType
 }
 
 export const preloadAsset = ({
-  asset,
+  url,
   onProgress,
   onComplete,
   onError,
   responseType = 'blob',
 }: PreloadAssetProps) => {
-  const xhr = new XMLHttpRequest()
-  xhr.open('GET', asset.url, true)
-  xhr.responseType = responseType
+  const asset = { url } as Asset
 
-  asset.xhr = xhr
+  const request = () => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', asset.url, true)
+    xhr.responseType = responseType
 
-  xhr.onprogress = (event) => {
-    if (event.lengthComputable) {
-      asset.progress = event.loaded / event.total
-      asset.downloaded = event.loaded
-      asset.total = event.total
-      onProgress(event)
+    asset.xhr = xhr
+
+    xhr.onprogress = (event) => {
+      if (event.lengthComputable) {
+        asset.progress = event.loaded / event.total
+        asset.downloaded = event.loaded
+        asset.total = event.total
+        onProgress({
+          progress: asset.progress,
+          event,
+          asset,
+        })
+      }
     }
-  }
 
-  xhr.onload = (event) => {
-    // TODO: fix
-    // @ts-expect-error
-    const type = event.target.response.type
-    // @ts-expect-error
-    const responseURL = event.target.responseURL
-
-    asset.fileName = responseURL.substring(responseURL.lastIndexOf('/') + 1)
-    asset.type = type
-    asset.status = xhr.status
-
-    if (xhr.status == 404) {
-      asset.blobUrl = asset.size = null
-      asset.error = true
-      onError(asset)
-    } else {
+    xhr.onload = (event) => {
       // TODO: fix
       // @ts-expect-error
-      const blob = new Blob([event.target.response], { type })
-      asset.blobUrl = URL.createObjectURL(blob)
-      asset.size = blob.size
-      asset.error = false
+      const type = event.target.response.type
+      // @ts-expect-error
+      const responseURL = event.target.responseURL
+
+      asset.fileName = responseURL.substring(responseURL.lastIndexOf('/') + 1)
+      asset.type = type
+      asset.status = xhr.status
+
+      if (xhr.status == 404) {
+        asset.blobUrl = asset.size = null
+        asset.error = true
+        onError(asset)
+      } else {
+        // TODO: fix
+        // @ts-expect-error
+        const blob = new Blob([event.target.response], { type })
+        asset.blobUrl = URL.createObjectURL(blob)
+        asset.size = blob.size
+        asset.error = false
+      }
+      onComplete(asset)
     }
-    onComplete(asset)
+    xhr.send()
   }
-  xhr.send()
+
+  request()
+
+  return asset
 }
